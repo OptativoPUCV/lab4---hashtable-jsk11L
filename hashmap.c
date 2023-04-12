81b38542ebd3cdf88efc25389e543991885e9201
Replit user <>
2023-04-12 22:32:11 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -45,7 +45,7 @@ void insertMap(HashMap * map, char * key, void * value) {
 }
 
 void enlarge(HashMap * map) {
-    enlarge_called = 1; //no borrar (testing purposes)
+    enlarge_called = 1; //no borrar (testing purposes PLEASE JUST PLEASE JUST PLEASE)
 
 
 }
